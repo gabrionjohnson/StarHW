ce5c4227c4321b3ded97e14e90afb0204ea6c59a
Gabrion Johnson <johnsongabrion@gmail.com>
2020-10-13 00:12:52 -0400

    init commit

---
+++ src/Homework.java
@@ -0,0 +1,30 @@
+public class Homework {
+    public static void main(String[] args) {
+
+
+        // int k = 80;
+        // for (int i = 0; i < 5; i++){
+        //     k = k-12;
+        // } its going to run this code 5 times 
+        // (0,1,2,3, until i = 4 bc thats the last number thats less than 5)
+        // each time it subtracts 12 from the new 'i' (80-12=68. then 68-12=56 )
+        // 68,56,44,32,20
+        // so following the loop, k would end up being 20
+
+
+
+
+
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j<= i; j++){
+            System.out.print('*');
+                 }
+                 System.out.println();
+             }
+
+    
+        }
+
+
+}
